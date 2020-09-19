1c72e525023926f924bac144f47db9e39bdb2c78
Shreya Suroliya <suroliya.shreya2014@gmail.com>
2020-09-19 19:01:25 +0530

    checkboxes tried

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,140 @@
 var counter = 2;
-var limit = 6;
-function addInput(divName){
-     if (counter == limit)  {
-          alert("You have reached the limit of adding " + counter + " inputs");
-     }
-     else {
-          var newdiv = document.createElement('div');
-          newdiv.innerHTML = "Person " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
-          document.getElementById(divName).appendChild(newdiv);
-          counter++;
-     }
+var limit = 5;
+
+function addInput(divName) {
+    if (counter == limit) {
+        alert("You have reached the limit of adding " + counter + " inputs");
+    } else {
+        var newdiv = document.createElement('div');
+        newdiv.innerHTML = "Person " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+        document.getElementById(divName).appendChild(newdiv);
+        counter++;
+    }
     var ansD = document.getElementById("tot");
-		ansD.value = counter;
-    
+    ansD.value = counter;
+
 }
-function val(){
-    
+
+function Hide() {
+    document.getElementById("checkbox1").style.display = "none";
+    document.getElementById("checkbox2").style.display = "none";
+    document.getElementById("checkbox3").style.display = "none";
+    document.getElementById("checkbox4").style.display = "none";
+    document.getElementById("checkbox5").style.display = "none";
+
+}
+
+function ShowHide() {
+    var counter = parseInt(document.getElementById("tot").value);
+    alert("Either enter Amount to be divided or let us divide equally among selected people");
+    if (counter == 2) {
+        document.getElementById("checkbox1").style.display = "block";
+        document.getElementById("checkbox2").style.display = "block";
+
+    } else if (counter == 3) {
+        document.getElementById("checkbox1").style.display = "block";
+        document.getElementById("checkbox2").style.display = "block";
+        document.getElementById("checkbox3").style.display = "block";
+
+    }
+    if (counter == 4) {
+        document.getElementById("checkbox1").style.display = "block";
+        document.getElementById("checkbox2").style.display = "block";
+        document.getElementById("checkbox3").style.display = "block";
+        document.getElementById("checkbox4").style.display = "block";
+
+    }
+    if (counter == 5) {
+        document.getElementById("checkbox1").style.display = "block";
+        document.getElementById("checkbox2").style.display = "block";
+        document.getElementById("checkbox3").style.display = "block";
+        document.getElementById("checkbox4").style.display = "block";
+        document.getElementById("checkbox5").style.display = "block";
+
+    }
 }
 
 function exp() {
-    var exclude = parseInt(document.getElementById("choice").value);
+    var selected = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    alert("selected checkboxes are " + selected);
+
+
     var counter = parseInt(document.getElementById("tot").value);
+    var exclude = counter - selected;
     var expense = parseInt(document.getElementById("expense").value);
     
-    var co = parseInt(document.getElementById("cost").value);
-    var cal = parseInt(document.getElementById("calex").value);
+        var co = parseInt(document.getElementById("cost").value);
+        var cal = parseInt(document.getElementById("calex").value);
+
+        var present = expense / (counter - exclude);
+
+        var calex = document.getElementById("calex");
+        calex.value = cal + expense;
+
+        var cost = document.getElementById("cost");
+        cost.value = present + co;
+
+        var res = document.getElementById("res");
+        res.value = parseInt(document.getElementById("calex").value) - parseInt(document.getElementById("cost").value);
+
+        var paid = document.getElementById("paid");
+        paid.value = parseInt(document.getElementById("calex").value);
+        alert("This Expense Added !!! Add another or Calculate Total");
+
+
     
-    var present = expense/(counter-exclude);
-    
-    var calex = document.getElementById("calex");
-    calex.value = cal + expense;
-    
-    var cost = document.getElementById("cost");
-    cost.value = present + co;
-    
-    var res = document.getElementById("res");
-    res.value = parseInt(document.getElementById("calex").value) -  parseInt(document.getElementById("cost").value);
-    
-    var paid = document.getElementById("paid");
-    paid.value = parseInt(document.getElementById("calex").value);
-    alert("This Expense Added !!! Add another or Calculate Total");
-    
-    var change = document.getElementById("choice");
-    choice.value = "0";
-    
+
+
+
 }
 
 function showpanel() {
     let allAreFilled = true;
-  document.getElementById("main").querySelectorAll("[required]").forEach(function(i) {
-    if (!allAreFilled) return;
-    if (!i.value) allAreFilled = false;
-    if (i.type === "radio") {
-      let radioValueCheck = false;
-      document.getElementById("myForm").querySelectorAll(`[name=${i.name}]`).forEach(function(r) {
-        if (r.checked) radioValueCheck = true;
-      })
-      allAreFilled = radioValueCheck;
-    }
-  })
-  if (!allAreFilled) {
-    alert('Fill all the fields');
-  }
-    else{
+    document.getElementById("main").querySelectorAll("[required]").forEach(function (i) {
+        if (!allAreFilled) return;
+        if (!i.value) allAreFilled = false;
+        if (i.type === "radio") {
+            let radioValueCheck = false;
+            document.getElementById("myForm").querySelectorAll(`[name=${i.name}]`).forEach(function (r) {
+                if (r.checked) radioValueCheck = true;
+            })
+            allAreFilled = radioValueCheck;
+        }
+    })
+    if (!allAreFilled) {
+        alert('Fill all the fields');
+    } else {
         document.getElementById("panel").style.display = "block";
         document.getElementById("main").style.display = "none";
     }
-  
+
 }
 
 function showpanel2() {
     let allAreFilled = true;
-  document.getElementById("main").querySelectorAll("[required]").forEach(function(i) {
-    if (!allAreFilled) return;
-    if (!i.value) allAreFilled = false;
-    if (i.type === "radio") {
-      let radioValueCheck = false;
-      document.getElementById("myForm").querySelectorAll(`[name=${i.name}]`).forEach(function(r) {
-        if (r.checked) radioValueCheck = true;
-      })
-      allAreFilled = radioValueCheck;
-    }
-  })
-  if (!allAreFilled) {
-    alert('Fill all the fields');
-  }
-    else{
-  document.getElementById("panelex").style.display = "block";
-  document.getElementById("panel").style.display = "none";
+    document.getElementById("main").querySelectorAll("[required]").forEach(function (i) {
+        if (!allAreFilled) return;
+        if (!i.value) allAreFilled = false;
+        if (i.type === "radio") {
+            let radioValueCheck = false;
+            document.getElementById("myForm").querySelectorAll(`[name=${i.name}]`).forEach(function (r) {
+                if (r.checked) radioValueCheck = true;
+            })
+            allAreFilled = radioValueCheck;
+        }
+    })
+    if (!allAreFilled) {
+        alert('Fill all the fields');
+    } else {
+        document.getElementById("panelex").style.display = "block";
+        document.getElementById("panel").style.display = "none";
 
     }
 }
+
 function showpanelback() {
     choice.value = "0";
     document.getElementById("panel").style.display = "block";
     document.getElementById("panelex").style.display = "none";
-  
+
 }
